541d93317fa040a1d0816da8a0807e9ab40653a4
David <me@example.com>
2024-11-20 20:12:45 -0500

    clases CajeraProductoHilos.java y ClienteCajeraHilosMain

---
+++ src/main/java/com/iudigital/actividad/conhilos/CajeraProductoHilos.java
@@ -0,0 +1,2 @@
+package com.iudigital.actividad.conhilos;
+
---
+++ src/main/java/com/iudigital/actividad/conhilos/ClienteCajeraHilosMain.java
@@ -0,0 +1,6 @@
+package com.iudigital.actividad.conhilos;
+
+public class ClienteCajeraHilosMain {
+
+    public static void main(String[] args) {}
+}
